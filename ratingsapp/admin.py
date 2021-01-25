467f2042e7c763d3440d35bda9b00cc7f67a4fbc
rickymwethera <rickymwethera@gmail.com>
2021-01-26 00:36:12 +0300

    created superuser

--- ratingsapp/admin.py
+++ ratingsapp/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Project, Rate
 
 # Register your models here.
+
+admin.site.register(Project)
+admin.site.register(Profile)
+admin.site.register(Rate)
