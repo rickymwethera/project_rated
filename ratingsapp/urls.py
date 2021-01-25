17c371fb23ee44219396411e3fa2733092bd7414
rickymwethera <rickymwethera@gmail.com>
2021-01-26 00:37:19 +0300

    added media root

--- ratingsapp/urls.py
+++ ratingsapp/urls.py
@@ -3,7 +3,12 @@ from django.conf.urls import url
 from . import views
 from django.conf import settings
 from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.home, name='home'),
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
