432c73e9fd8d4b089b0e7e0b3a4a03c4d6ccae62
rickymwethera <rickymwethera@gmail.com>
2021-01-26 01:08:47 +0300

    added search url

--- ratingsapp/urls.py
+++ ratingsapp/urls.py
@@ -8,6 +8,8 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     path('',views.home, name='home'),
+    path('project/new/',views.create_post,name = 'create'),
+    path('^search/', views.search_results, name='search'),
 ]
 
 if settings.DEBUG:
