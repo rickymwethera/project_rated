f73486dc356be4b8444dab47c1374cde5809b1a5
rickymwethera <rickymwethera@gmail.com>
2021-01-26 00:23:19 +0300

    added database settings

--- ratings/settings.py
+++ ratings/settings.py
@@ -79,11 +79,12 @@ WSGI_APPLICATION = 'ratings.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ratings',
+        'USER': 'ricky',
+    'PASSWORD':'mwethera99',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
