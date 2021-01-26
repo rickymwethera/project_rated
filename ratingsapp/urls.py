7cae0d046a39ab573c07897e536e6c7d8aeb96d2
rickymwethera <rickymwethera@gmail.com>
2021-01-26 03:00:57 +0300

    added details url

--- ratingsapp/urls.py
+++ ratingsapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns=[
     path('',views.home, name='home'),
     path('project/new/',views.create_post,name = 'create'),
     path('^search/', views.search_results, name='search'),
+    url(r'project/(\d+)',views.detail,name = 'details'),
 ]
 
 if settings.DEBUG:
